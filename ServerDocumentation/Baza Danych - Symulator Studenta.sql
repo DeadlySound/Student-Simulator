1ded264c7e0d2aab5886eaa95435bfbd7c55687e
Adam Lewandowski <64542253+DeadlySound@users.noreply.github.com>
2021-08-15 20:04:25 +0200

    Adjustments to database
    Various Adjustments to database:
    - Constraint adjustment To 'Grades' table
    - Second script loading data into database (2 tables not implemented)
    - csv files for every table containing data

--- ServerDocumentation/Baza Danych - Symulator Studenta.sql
+++ ServerDocumentation/Baza Danych - Symulator Studenta.sql
@@ -1,6 +1,6 @@
 -- Lewandowski Adam 
 -- 14.08.2021
--- Temat: Obs³uga sieci wyci¹gów narciarskich
+-- Topic: Student-Simulator
 
 USE master
 GO
@@ -12,7 +12,7 @@ CREATE DATABASE Student_Simulator
 ON
 (
 	NAME = Student_Simulator,
-	FILENAME = 'E:\Pendrive\Notatki\02. Wyk³ady\IV Semestr\PIV\Symulator Studenta\Student_Simulator.mdf',
+	FILENAME = 'E:\Pendrive\Notatki\02. Wyk³ady\IV Semestr\PIV\Symulator Studenta\Student-Simulator\Student-Simulator\ServerDocumentation\Student_Simulator.mdf',
 	SIZE = 10,
 	MAXSIZE = 50,
 	FILEGROWTH = 5
@@ -20,7 +20,7 @@ ON
 LOG ON
 (
 	NAME = Student_Simulator_log,
-	FILENAME = 'E:\Pendrive\Notatki\02. Wyk³ady\IV Semestr\PIV\Symulator Studenta\Symulator_Studenta_log.ldf',
+	FILENAME = 'E:\Pendrive\Notatki\02. Wyk³ady\IV Semestr\PIV\Symulator Studenta\Student-Simulator\Student-Simulator\ServerDocumentation\Symulator_Studenta_log.ldf',
 	SIZE = 5MB,
 	MAXSIZE = 25MB,
 	FILEGROWTH = 5MB
@@ -41,9 +41,9 @@ GO
 
 USE Student_Simulator
 GO
-DROP TABLE IF EXISTS School;			-- "Katedra" in Polish (I hope)
+DROP TABLE IF EXISTS Department;
 GO
-CREATE TABLE School
+CREATE TABLE Department
 (
 ID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 [Name] nvarchar(128) NOT NULL
@@ -53,7 +53,7 @@ GO
 
 USE Student_Simulator
 GO
-DROP TABLE IF EXISTS Field_of_study;			-- "Kierunek studiów" in Polish (I hope)
+DROP TABLE IF EXISTS Field_of_study;
 GO
 CREATE TABLE Field_of_study
 (
@@ -74,7 +74,7 @@ ID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 Hours_per_week int NOT NULL,
 Field_of_study_ID int NOT NULL FOREIGN KEY REFERENCES Field_of_study(ID), 
 Term int NOT NULL,															-- "Semestr"
-School_ID int NOT NULL FOREIGN KEY REFERENCES School(ID)
+Department_ID int NOT NULL FOREIGN KEY REFERENCES Department(ID)
 )
 GO
 
@@ -100,7 +100,7 @@ ID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 First_Name nvarchar(32) NOT NULL,
 Last_Name nvarchar(32) NOT NULL,
 Character_ID int NOT NULL,											
-School_ID int NOT NULL FOREIGN KEY REFERENCES School(ID)
+Department_ID int NOT NULL FOREIGN KEY REFERENCES Department(ID)
 )
 GO
 
@@ -113,7 +113,7 @@ CREATE TABLE Subject_Assignment
 ID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 Employee_ID int NOT NULL FOREIGN KEY REFERENCES Employee(ID),
 Subject_ID int NOT NULL FOREIGN KEY REFERENCES [Subject](ID),
-Academic_Year nvarchar(7) NOT NULL
+Academic_Year nvarchar(10) NOT NULL
 )
 GO
 
@@ -126,6 +126,17 @@ CREATE TABLE Grades
 ID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 Subject_ID int NOT NULL FOREIGN KEY REFERENCES [Subject](ID),
 Student_ID int NOT NULL FOREIGN KEY REFERENCES Student(ID),
-Grade int NULL 
+Grade float(24) NULL 
 )
+GO
+
+
+-- data constraints
+
+USE Student_Simulator
+GO
+ALTER TABLE dbo.Grades
+	DROP CONSTRAINT IF EXISTS check_grade
+ALTER TABLE dbo.Grades
+   ADD CONSTRAINT check_grade CHECK (Grade IN (NULL, 2, 3, 3.5, 4, 4.5 ,5) ) ;
 GO
